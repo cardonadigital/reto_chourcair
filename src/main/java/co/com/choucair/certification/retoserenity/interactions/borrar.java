d28494bb5ae42018968596b6c8d6aacd76dedd61
Daniel Cardona <elaportador11@gmail.com>
2023-09-09 17:09:29 -0500

    creation of the archetype

---
+++ src/main/java/co/com/choucair/certification/retoserenity/interactions/borrar.java
@@ -0,0 +1,4 @@
+package co.com.choucair.certification.retoserenity.interactions;
+
+public class borrar {
+}
---
+++ src/main/java/co/com/choucair/certification/retoserenity/models/borrar.java
@@ -0,0 +1,4 @@
+package co.com.choucair.certification.retoserenity.models;
+
+public class borrar {
+}
